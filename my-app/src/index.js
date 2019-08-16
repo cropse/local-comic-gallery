66c7c4594a2fccd62f6b2e694edc8f7724c7959a
cropse <stu0219@hotmail.com>
2019-08-16 23:14:38 +0000

    function work and formated.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,44 +4,7 @@ import './index.css';
 
 // import { photos } from "./photos";
 // import ReactFileReader from 'react-file-reader';
-import {MainList} from './fetch.js';
-// import {Gallerybundle} from './gallery.js'
-// import Files from 'react-files'
-
-// const photos = [
-//   {
-//     src: 'http://localhost:8888/test/1.jpg',
-//     width: 3,
-//     height: 4
-//   },
-//   {
-//     src: 'http://localhost:8888/test/2.jpg',
-//     width: 3,
-//     height: 4
-//   },
-//   {
-//     src: 'http://localhost:8888/test/3.jpg',
-//     width: 3,
-//     height: 4
-//   },
-//   {
-//     src: 'http://localhost:8888/test/4.jpg',
-//     width: 3,
-//     height: 4
-//   },
-//   {
-//     src: 'http://localhost:8888/test/5.jpg',
-//     width: 3,
-//     height: 4
-//   },
-//   {
-//     src: 'http://localhost:8888/test/6.jpg',
-//     width: 3,
-//     height: 4
-//   },
-// ];
-
-
+import {MainList} from './page.js';
 
 function App(props){
   return(
